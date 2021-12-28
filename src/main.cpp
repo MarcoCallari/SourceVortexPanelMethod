015fcc7c909dbe6b6329b068aa5abc4f1528a7a3
Marco <a@a.it>
2021-12-28 22:33:54 +0100

    Cleanup. Remove trapezoidal integral.

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,22 @@
 #include <gsl/gsl_integration.h>
 
 
+struct SigmaIntegralParameters {
+  const Panel* panelI;
+  const Panel* panelJ;
+};
+
 //s \in [0,Panel.length()]
-inline double integral(const Panel& panelI, const Panel& panelJ, const double s) {
-    const double xi = panelI.controlPoint().x;
-    const double yi = panelI.controlPoint().y;
-    const double deltai = panelI.delta();
-    const auto t = (s / panelJ.length());
-    const double xj = (1 - t) * panelJ.startPoint().x + t * panelJ.endPoint().x;
-    const double yj = (1 - t) * panelJ.startPoint().y + t * panelJ.endPoint().y;
+inline double sigmaIntegral(const double s, void* params) {
+    const auto p = reinterpret_cast<SigmaIntegralParameters*>(params);
+    const auto panelI = p->panelI;
+    const auto panelJ = p->panelJ;
+    const double xi = panelI->controlPoint().x;
+    const double yi = panelI->controlPoint().y;
+    const double deltai = panelI->delta();
+    const auto t = (s / panelJ->length());
+    const double xj = (1 - t) * panelJ->startPoint().x + t * panelJ->endPoint().x;
+    const double yj = (1 - t) * panelJ->startPoint().y + t * panelJ->endPoint().y;
     return ((xi-xj)*std::cos(deltai)+(yi-yj)*std::sin(deltai)) /
             ((xi-xj)*(xi-xj)+(yi-yj)*(yi-yj));
 }
@@ -44,23 +52,6 @@ inline double velocityIntegrandY(const Point& point, const Panel& panelJ, const 
     const double yj = (1 - t) * panelJ.startPoint().y + t * panelJ.endPoint().y;
     return (yi-yj) /
             ((xi-xj)*(xi-xj)+(yi-yj)*(yi-yj));
-}
-
-template <typename F>
-inline double trapezoidal(const F& func, const double min, const double max, const double step) {
-    double result = 0;
-    for(double val = min; val < max - step; val += step) {
-            result += ((func(val) + func(val+step)) / 2.0)*(step);
-    }
-    return result;
-}
-
-template <class T>
-std::ostream& operator<<(std::ostream& stream, const std::vector<T>& objects) {
-    for (const auto& obj : objects) {
-        stream << obj << "\n";
-    }
-    return stream;
 }
 
 struct VelocityIntegralParameters {
@@ -97,14 +88,18 @@ int main(int argv, char** argc) {
     Eigen::Matrix<double, panelsNumber, 1> b;
     std::ofstream file_a("./out_a.txt");
     assert(file_a.good());
+    auto* w = gsl_integration_workspace_alloc(1'000);
     for(size_t i = 0; i < test->panels().size(); ++i){ 
         for(size_t j = 0; j < test->panels().size(); ++j) {
             if(i!=j) {
-                const auto& panelI = test->panels().at(i);
-                const auto& panelJ = test->panels().at(j);
-                A(i, j) = 1/(2.0*M_PI) * trapezoidal([&panelI, &panelJ](const double s) { 
-                        return integral(panelI, panelJ, s);
-                        }, 0.0, panelJ.length(), 1.0/10'000.0); 
+                SigmaIntegralParameters p { &test->panels()[i], &test->panels()[j] };
+                gsl_function integrand;
+                integrand.function = sigmaIntegral;
+                integrand.params = reinterpret_cast<void*>(&p);
+                double result = 0;
+                double abserr = 0;
+                gsl_integration_qags(&integrand, 0, test->panels()[j].length(), 1.49e-8, 1.49e-8, 1'000, w, &result, &abserr);
+                A(i, j) = 1 / (2.0 * M_PI) * result;
             }
             else {
                 A(i, j)= 0.5;
@@ -139,9 +134,6 @@ int main(int argv, char** argc) {
     u.setOnes(numPoints, numPoints);
     Eigen::Matrix<double, numPoints, numPoints> v;
     v.setZero(numPoints, numPoints);
-    double maxerrX = 0.0;
-    double maxerrY = 0.0;
-    auto* w = gsl_integration_workspace_alloc(1'000);
     for (size_t indexY = 0; indexY < numPoints; ++indexY) {
         for (size_t indexX = 0; indexX < numPoints; ++indexX) {
             for (const auto& panel : test->panels()) {
